966add77544ae254986e53334d3d7b91cb396e8f
João Pedro <126018999+jotapedz@users.noreply.github.com>
2023-04-11 13:35:38 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿var num = 0;
+var maior = 0;
+var menor = 10000;
+
+do
+{
+    Console.WriteLine("Informe um numero(0 para sair):");
+    num= int.Parse(Console.ReadLine());
+   if (num > maior)
+       maior = num; 
+   else if (num < menor)
+       menor = num;
+
+
+   if (num == 0)
+    {
+        Console.WriteLine($"O maior numero é {maior}");
+        Console.WriteLine($"O menor numero é {menor}");
+        
+    }
+   
+} while(num != 0);
+    
